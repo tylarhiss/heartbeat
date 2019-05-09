5e11476166519ef3926d98cc9ffef7c11ded7a50
Tylar Hiss <tylar.hiss@navitaire.com>
2019-05-09 13:08:45 -0600

    starting heartbeat

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App: React.FC = () => {
+  const [shouldHeartbeat, setShouldHeartbeat] = useState(false);
+  const [host, setHost] = useState('');
+  const [token, setToken] = useState('');
+
+  const test = (
+    <>
+      <input
+        type="text"
+        name=""
+        id=""
+        value={token}
+        onChange={e => setToken(e.target.value)}
+      />
+      <input
+        type="text"
+        name=""
+        id=""
+        value={host}
+        onChange={e => setHost(e.target.value)}
+      />
+    </>
+  );
+
+  useEffect(() => {
+    if (!shouldHeartbeat) {
+      return;
+    }
+
+    let timerTimeout = setTimeout(() => {
+      console.log('test');
+    }, 60000);
+
+    return () => {
+      if (timerTimeout) {
+        clearTimeout(timerTimeout);
+      }
+    };
+  }, [shouldHeartbeat]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!shouldHeartbeat && test}
+
+        <button onClick={() => setShouldHeartbeat(b => !b)}>
+          {!shouldHeartbeat ? 'Start' : 'Stop'}
+        </button>
+
+        {shouldHeartbeat && <div className="heart" />}
       </header>
     </div>
   );
-}
+};
 
 export default App;
